f3bbfc054556850d0b6a4891e4dd832044895135
KUCKLU <hello@kuck1u.me>
2018-12-01 19:31:21 +0900

    no message

--- includes/class-lazyload.php
+++ includes/class-lazyload.php
@@ -3,7 +3,7 @@
  * PHP 5.6 or later
  *
  * @package    KALEIDPIXEL
- * @author     KUCKLU <hello@kuck1u.me>
+ * @author     KUCKLU <oss@kaleidpixel.jp>
  * @copyright  2018 Kaleid Pixel
  * @license    GNU General Public License v2.0 or later version
  * @version    0.0.1
--- kp-image-optimizer.php
+++ kp-image-optimizer.php
@@ -4,31 +4,17 @@
  * Plugin URI: https://github.com/kaleidpixel/wp-image-optimizer
  * Description: This plugin might have the power to shift the world beyond Divergence 1%.
  * Author: Kaleid Pixel (KUCKLU)
- * Version: 0.0.1
+ * Version: 0.1.0
  * Author URI: https://www.kaleidpixel.jp
  * Domain Path: /languages
  * Text Domain: kp-image-optimizer
- * License: GNU General Public License v3.0
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * License: GNU General Public License v2.0 or later version
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package    WordPress
  * @subpackage KP Image Optimizer
  * @since      0.0.1
- * @author     KUCKLU <hello@kuck1u.me>
- *             Copyright (C) 2018 Kaleid Pixel
- *
- *             This program is free software: you can redistribute it and/or modify
- *             it under the terms of the GNU General Public License as published by
- *             the Free Software Foundation, either version 3 of the License, or
- *             (at your option) any later version.
- *
- *             This program is distributed in the hope that it will be useful,
- *             but WITHOUT ANY WARRANTY; without even the implied warranty of
- *             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *             GNU General Public License for more details.
- *
- *             You should have received a copy of the GNU General Public License
- *             along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * @author     KUCKLU <oss@kaleidpixel.jp>
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
